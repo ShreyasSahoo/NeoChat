fd1427c1440f4c39d9db2cc20377a98bc705ace3
Shreyas Sahoo <shreyassahoo26@gmail.com>
2025-10-22 15:14:09 +0530

    [Feature] Add SwiftLint

--- NeoChatUITests/NeoChatUITestsLaunchTests.swift
+++ NeoChatUITests/NeoChatUITestsLaunchTests.swift
@@ -9,7 +9,7 @@ import XCTest
 
 final class NeoChatUITestsLaunchTests: XCTestCase {
 
-    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+    override static var runsForEachTargetApplicationUIConfiguration: Bool {
         true
     }
 
