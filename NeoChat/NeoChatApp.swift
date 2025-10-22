6a1ebd8bb5f2529deb15a7db015b05850b8c37dc
Shreyas Sahoo <shreyassahoo26@gmail.com>
2025-10-22 13:10:25 +0530

    Initial Commit

---
+++ NeoChat/NeoChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeoChatApp.swift
+//  NeoChat
+//
+//  Created by Shreyas on 22/10/25.
+//
+
+import SwiftUI
+
+@main
+struct NeoChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NeoChatTests/NeoChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  NeoChatTests.swift
+//  NeoChatTests
+//
+//  Created by Shreyas on 22/10/25.
+//
+
+import Testing
+@testable import NeoChat
+
+struct NeoChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
