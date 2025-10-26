fabe67c51a43a070e09ac8ea6df7527964be41e7
Shreyas Sahoo <shreyassahoo26@gmail.com>
2025-10-26 14:35:12 +0530

    [Feature] Add CarouselView

--- NeoChat/Components/Views/CarouselView.swift
+++ NeoChat/Components/Views/CarouselView.swift
@@ -7,12 +7,69 @@
 
 import SwiftUI
 
-struct CarouselView: View {
+struct CarouselView<Content: View, T: Hashable>: View {
+
+    var items: [T]
+    @ViewBuilder var content: (T) -> Content
+    @State private var selection: T?
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 8) {
+            scrollView
+
+            HStack(spacing: 8) {
+                ForEach(items, id: \.self) { item in
+                    Circle()
+                        .fill(selection == item ? Color.accent : .secondary)
+                        .frame(width: selection == item ? 10 : 8, height: selection == item ? 10 : 8)
+                }
+            }
+        }
+    }
+
+    private var scrollView: some View {
+        ScrollView(.horizontal) {
+            LazyHStack(spacing: 0) {
+                ForEach(items, id: \.self) { item in
+                        content(item)
+                        .id(item)
+                        .frame(width: 300)
+                        .padding(16)
+                        .containerRelativeFrame(.horizontal, alignment: .center)
+                        .scrollTransition(.interactive.threshold(.visible(0.95))) { content, phase in
+                            content
+                                .scaleEffect(phase.isIdentity ? 1 : 0.9)
+                        }
+                }
+            }
+        }
+        .scrollIndicators(.hidden)
+        .scrollBounceBehavior(.basedOnSize)
+        .scrollTargetLayout()
+        .scrollTargetBehavior(.paging)
+        .frame(height: 200)
+        .scrollPosition(id: $selection)
+        .onAppear {
+            updateSelectionIfNeeded()
+        }
+        .onChange(of: items.count) {
+            updateSelectionIfNeeded()
+        }
+    }
+
+    private func updateSelectionIfNeeded() {
+        if selection == nil || selection == items.last {
+            selection = items.first
+        }
     }
 }
 
 #Preview {
-    CarouselView()
+    CarouselView(items: AvatarModel.mocks) { item in
+        HeroCellView(
+            title: item.name,
+            subtitle: item.characterDescription,
+            imageName: item.profileImageName
+        )
+    }
 }
