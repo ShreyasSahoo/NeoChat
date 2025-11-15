afcf92b92cd481f2acc6c31f2e4085935ce005f0
Shreyas Sahoo <shreyassahoo26@gmail.com>
2025-11-15 18:11:45 +0530

    [Feature] CategoryListView

---
+++ NeoChat/Core/CategoryList/CategoryListView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryListView.swift
+//  NeoChat
+//
+//  Created by Shreyas on 15/11/25.
+//
+
+import SwiftUI
+
+struct CategoryListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryListView()
+}
